af2922e6d975fe4d94a451a2f824f3ccbcc70e35
hdrake <henrifdrake@gmail.com>
2020-01-31 17:17:19 -0500

    Wrote some example notebooks that use my utility functions to make basic plots

--- notebooks/analysis/bbl_utils.py
+++ notebooks/analysis/bbl_utils.py
@@ -5,21 +5,14 @@ def z_mean_profile(da):
     da_m = da.where(da != 0.)
     return da_m.mean(dim=['XC', 'YC'])
 
+def weighted_mean(da, weights, **kwargs):
+    return (da*weights).sum(**kwargs)/weights.sum(**kwargs)
+
 def pre_process(ds, timestep=60.):
     ds['time'] = ds['time']*timestep # convert time from iterations to seconds
     
     coords = ds.coords.to_dataset().reset_coords()
     ds = ds.reset_coords(drop=True)
-
-    H = coords['Depth'].max().copy()
-    ds['Z'].values = ds['Z'].values + H.values
-    ds['Zl'].values = ds['Zl'].values + H.values
-    ds['Zu'].values = ds['Zu'].values + H.values
-    ds['Zp1'].values = ds['Zp1'].values + H.values
-    coords['Z'].values = coords['Z'].values + H.values
-    coords['Zl'].values = coords['Zl'].values + H.values
-    coords['Zu'].values = coords['Zu'].values + H.values
-    coords['Zp1'].values = coords['Zp1'].values + H.values
     
     # Add grid metrics
     coords['drW'] = coords.hFacW * coords.drF #vertical cell size at u point
@@ -42,6 +35,20 @@ def pre_process(ds, timestep=60.):
         ('X', 'Y'): ['rA', 'rAz', 'rAs', 'rAw'] # Areas
     }
     grid = Grid(coords, metrics=metrics, periodic=['X','Y'])
+    
+    H = coords['Depth'].max().values.copy().astype('>f8')
+    ds = ds.assign_coords(
+        Z = ds['Z'] + H,
+        Zl = ds['Zl'] + H,
+        Zu = ds['Zu'] + H,
+        Zp1 = ds['Zp1'] + H
+    )
+    coords = coords.assign_coords(
+        Z = coords['Z'] + H,
+        Zl = coords['Zl'] + H,
+        Zu = coords['Zu'] + H,
+        Zp1 = coords['Zp1'] + H
+    )
 
     return ds, coords, grid
 
@@ -53,9 +60,10 @@ def add_cartesian_coordinates(ds, coords, θ):
         'Zr_V': (['XC', 'Z'], (ds['XC']*np.sin(θ) + ds['Z']*np.cos(θ)).values),
         'Xr_V': (['XC', 'Z'], (ds['XC']*np.cos(θ) - ds['Z']*np.sin(θ)).values),
         'Zr_U': (['XG', 'Z'], (ds['XG']*np.sin(θ) + ds['Z']*np.cos(θ)).values),
-        'Xr_U': (['XG', 'Z'], (ds['XG']*np.cos(θ) - ds['Z']*np.sin(θ)).values)
+        'Xr_U': (['XG', 'Z'], (ds['XG']*np.cos(θ) - ds['Z']*np.sin(θ)).values),
+        'Zr_W': (['XC', 'Zl'], (ds['XC']*np.sin(θ) + ds['Zl']*np.cos(θ)).values),
+        'Xr_W': (['XC', 'Zl'], (ds['XC']*np.cos(θ) - ds['Zl']*np.sin(θ)).values)
     })
-
 
     coords = coords.assign_coords({
         'Zr': (['XC', 'Z'], (coords['XC']*np.sin(θ) + coords['Z']*np.cos(θ)).values),
@@ -63,7 +71,9 @@ def add_cartesian_coordinates(ds, coords, θ):
         'Zr_V': (['XC', 'Z'], (coords['XC']*np.sin(θ) + coords['Z']*np.cos(θ)).values),
         'Xr_V': (['XC', 'Z'], (coords['XC']*np.cos(θ) - coords['Z']*np.sin(θ)).values),
         'Zr_U': (['XG', 'Z'], (coords['XG']*np.sin(θ) + coords['Z']*np.cos(θ)).values),
-        'Xr_U': (['XG', 'Z'], (coords['XG']*np.cos(θ) - coords['Z']*np.sin(θ)).values)
+        'Xr_U': (['XG', 'Z'], (coords['XG']*np.cos(θ) - coords['Z']*np.sin(θ)).values),
+        'Zr_W': (['XC', 'Zl'], (coords['XC']*np.sin(θ) + coords['Zl']*np.cos(θ)).values),
+        'Xr_W': (['XC', 'Zl'], (coords['XC']*np.cos(θ) - coords['Zl']*np.sin(θ)).values)
     })
 
     return ds, coords
@@ -73,13 +83,17 @@ def add_background_temp(ds, Γ):
     ds['THETA_BG'] = ds['THETA'] + ds['Zr'] * Γ
     return ds
     
-
-
 def add_hab_coordinates(ds, coords):
-    ds['Z_hab'].values = ds['Z'].values + coords['Depth']
-    coords['Z_hab'].values = coords['Z'].values + coords['Depth']
-    ds['Zl_hab'].values = ds['Zl'].values + coords['Depth']
-    coords['Zl_hab'].values = coords['Zl'].values + coords['Depth']
+    
+    ds = ds.assign_coords({
+        'Z_hab':  ds['Z'] + (coords['Depth'] - coords['Depth'].max()),
+        'Zl_hab': ds['Zl'] + (coords['Depth'] - coords['Depth'].max())
+    })
+
+    coords = coords.assign_coords({
+        'Z_hab':  coords['Z'] + (coords['Depth'] - coords['Depth'].max()),
+        'Zl_hab': coords['Zl'] + (coords['Depth'] - coords['Depth'].max())
+    })    
     return ds, coords
 
 def interp_to_hab_coords(ds):
